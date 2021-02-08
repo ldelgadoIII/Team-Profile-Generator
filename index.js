697c3818bb486e364421747b39e8ecccf6e3a9e8
Loreto Delgado III <ldelgadothethird@gmail.com>
2021-02-08 17:01:26 -0600

    added employee class

---
+++ index.js
@@ -0,0 +1,16 @@
+// DEPENCIES ==============================
+// STARTING DATA ==========================
+// Employee class
+class Employee {
+  constructor(id, email) {
+    this.id = id;
+    this.email = email;
+  }
+
+  printInfo() {
+    console.log(`ID: ${this.id}`);
+    console.log(`Email: ${this.email}`);
+  }
+}
+// FUNCTIONS ==============================
+// USER INTERACTIONS ======================

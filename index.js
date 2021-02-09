f38bcd2fe8737fd9efd0557d152e233b770e52d9
Loreto Delgado III <ldelgadothethird@gmail.com>
2021-02-08 23:16:57 -0600

    fixed subclass parameters. added feature that pushes manager into an array. added switch case that switches questions for next inquirer prompt

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 const generateHTML = require("./generateHTML");
-const Employee = require("./lib/Employee");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 
 // STARTING DATA ==========================
 // Questions to ask user
@@ -59,7 +61,7 @@ const engineerQs = [
   {
     type: "list",
     message: "Would you like to add another employee?",
-    name: "menu",
+    name: "option",
     choices: ["Engineer", "Intern", "Finish Team"],
   },
 ];
@@ -88,25 +90,88 @@ const internQs = [
   {
     type: "list",
     message: "Would you like to add another employee?",
-    name: "menu",
+    name: "option",
     choices: ["Engineer", "Intern", "Finish Team"],
   },
 ];
 
+const employeeArray = [];
+
 // FUNCTIONS ==============================
 // Function to write file
-function writeToFile(fileName, data) {
-  fs.appendFile(fileName, data, (err) =>
-    err ? console.error(err) : console.log("HTML Created!")
-  );
-}
+// function writeToFile(fileName, data) {
+//   fs.appendFile(fileName, data, (err) =>
+//     err ? console.error(err) : console.log("HTML Created!")
+//   );
+
+// loop through allEmployes in the array
+//create the template
+// appendn the template to the html
+
+// for (var i =0; i < allEMployee.length; i++){
+//     const newTemplat = template(allEMployee[i]);
+//     writeToFile(dsfsdf, newTemplate);
+// }
+// function template(data) {
+//     return `<p>dfsdfsdfsf</p>`
+// }
 
 // Function to initialize app
 function init() {
+  let listQueestion;
+
   inquirer.prompt(initialQs).then((response) => {
-    generateHTML(response);
-    // writeToFile("dist/index.html", generateHTML(response));
+    const newManager = new Manager(
+      response.name,
+      "Manager",
+      response.id,
+      response.email,
+      response.office
+    );
+
+    employeeArray.push(newManager);
+    console.log("The contents of the employee Array: ", employeeArray);
+    switch (response.option) {
+      case "Engineer":
+        listQueestion = engineerQs;
+        console.log("List Question: ", listQueestion);
+        break;
+      case "Intern":
+        listQueestion = internQs;
+        console.log("List Question: ", listQueestion);
+        break;
+      default:
+        process.exit();
+        break;
+    }
+
+    // employeeObject
+    // inquirer.prompt(listQueestion).then((response) => {
+    //     switch (response.option){
+    //         case "Engineer":
+    //             employeeObject = new Engineer(....)
+    //             array.push(employeeObject.template)
+    //             break;
+    //     }
+    // });
   });
 }
+
+// listQueestion
+
+// switch statemetn
+// listQuestion = ...
+
+// secodn inquirer
+// new ... ()
+
+// }
+
+// const newIntern = new Intern(res.sdasd, res,.asdqad)
+//     generateHTML(response);
+//     // writeToFile("dist/index.html", generateHTML(response));
+//   });
+// }
+
 // USER INTERACTIONS ======================
 init();
